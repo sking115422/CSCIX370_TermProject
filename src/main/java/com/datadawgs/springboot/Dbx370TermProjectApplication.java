27d483c8790ea58bf00732de524205001ae2d021
Spencer King <spencerd.king@gmail.com>
2021-04-30 23:01:55 -0400

    general commit

--- src/main/java/com/datadawgs/springboot/Dbx370TermProjectApplication.java
+++ src/main/java/com/datadawgs/springboot/Dbx370TermProjectApplication.java
@@ -2,12 +2,19 @@ package com.datadawgs.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Dbx370TermProjectApplication {
+public class Dbx370TermProjectApplication extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dbx370TermProjectApplication.class, args);
-	}
+		}
 
 }
