ebe936bdb3d7acbe7b287c25583160198b204aaa
shreenal59 <shreenal59@gmail.com>
2021-05-03 17:17:21 -0400

    finished two graphs

--- src/main/java/com/datadawgs/springboot/controller/AppController.java
+++ src/main/java/com/datadawgs/springboot/controller/AppController.java
@@ -28,8 +28,8 @@ public class AppController {
     }
 
     @RequestMapping("/iwm")
-    public String iwmPage(Model model, @RequestParam("startdate") String startdate, @RequestParam("enddate") String  enddate) throws ParseException {
-
+    public String iwmPage(Model model, @RequestParam("startdate") String startdate, @RequestParam("enddate") String  enddate, @RequestParam("amount") String amount) throws ParseException {
+        double value = Double.parseDouble(amount);
         List<StockInfo> stockInfo = dao.getStockInfo("iwm");
         model.addAttribute("stockInfo", stockInfo.get(0));
 
@@ -39,16 +39,22 @@ public class AppController {
         Date endDate = format.parse(enddate);
         List<Stock> stocks = dao.list("iwm",startDate, endDate);
 
+        Stock firstday = dao.get("iwm", startDate);
+        double numberOfStock = value/firstday.getClose();
+
         Stock[] stockArray = stocks.toArray(new Stock[stocks.size()]);
         System.out.println(stockArray.length);
         System.out.println(stocks.size());
         String[] dates = stocks.stream().map(t -> t.getDate()).toArray(String[]::new);
         Double[] closing = stocks.stream().map(t -> t.getClose()).toArray(Double[]::new);
+        Double[] money = stocks.stream().map(t -> t.getClose() * numberOfStock).toArray(Double[]::new);
         System.out.println(dates.length);
         System.out.println(closing.length);
         model.addAttribute("stock", stockArray);
+        model.addAttribute("money", money);
         model.addAttribute("dates", dates);
         model.addAttribute("closing", closing);
+        model.addAttribute("numberOfStock", numberOfStock);
 
 
         List<Stock> highestOpen = dao.highestOpen("iwm", startDate, endDate);
@@ -71,27 +77,38 @@ public class AppController {
     @GetMapping(path="/qqq")
     public String getVolQQQ (Model model, @RequestParam("startdate") String startdate, @RequestParam("enddate") String  enddate, @RequestParam("amount") String amount) throws ParseException {
         double value = Double.parseDouble(amount);
+        List<StockInfo> stockInfo = dao.getStockInfo("qqq");
+        model.addAttribute("stockInfo", stockInfo.get(0));
+
+
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Date startDate = format.parse(startdate);
         Date endDate = format.parse(enddate);
         List<Stock> stocks = dao.list("qqq",startDate, endDate);
 
+        Stock firstday = dao.get("qqq", startDate);
+        double numberOfStock = value/firstday.getClose();
+
         Stock[] stockArray = stocks.toArray(new Stock[stocks.size()]);
         System.out.println(stockArray.length);
         System.out.println(stocks.size());
-        Stock firstday = dao.get("qqq", startDate);
-        double numberOfStock = value/firstday.getClose();
         String[] dates = stocks.stream().map(t -> t.getDate()).toArray(String[]::new);
-        Double[] closing = stocks.stream().map(t -> t.getClose() * numberOfStock).toArray(Double[]::new);
+        Double[] closing = stocks.stream().map(t -> t.getClose()).toArray(Double[]::new);
+        Double[] money = stocks.stream().map(t -> t.getClose() * numberOfStock).toArray(Double[]::new);
+        System.out.println(dates.length);
+        System.out.println(closing.length);
+        model.addAttribute("stock", stockArray);
+        model.addAttribute("money", money);
+        model.addAttribute("dates", dates);
+        model.addAttribute("closing", closing);
+        model.addAttribute("numberOfStock", numberOfStock);
+
+
         List<Stock> highestOpen = dao.highestOpen("qqq", startDate, endDate);
         List<Stock> highestVol = dao.highestVol("qqq", startDate, endDate);
         List<Stock> highestClose = dao.highestClose("qqq", startDate, endDate);
         List<Stock> biggestIncrease = dao.biggestIncrease("qqq", startDate, endDate);
         List<Stock> biggestDecrease = dao.biggestDecrease("qqq", startDate, endDate);
-        System.out.println(dates.length);
-        System.out.println(closing.length);
-        model.addAttribute("dates", dates);
-        model.addAttribute("closing", closing);
         model.addAttribute("stocks", stocks);
         model.addAttribute("highestOpen", highestOpen);
         model.addAttribute("highestVol", highestVol);
@@ -99,9 +116,106 @@ public class AppController {
         model.addAttribute("biggestIncrease", biggestIncrease);
         model.addAttribute("biggestDecrease", biggestDecrease);
 
-        return "qqq";
+
+        return "iwm";
 
     }
+
+    @GetMapping(path="/spy")
+    public String getVolSPY (Model model, @RequestParam("startdate") String startdate, @RequestParam("enddate") String  enddate, @RequestParam("amount") String amount) throws ParseException {
+        double value = Double.parseDouble(amount);
+        List<StockInfo> stockInfo = dao.getStockInfo("spy");
+        model.addAttribute("stockInfo", stockInfo.get(0));
+
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = format.parse(startdate);
+        Date endDate = format.parse(enddate);
+        List<Stock> stocks = dao.list("spy",startDate, endDate);
+
+        Stock firstday = dao.get("spy", startDate);
+        double numberOfStock = value/firstday.getClose();
+
+        Stock[] stockArray = stocks.toArray(new Stock[stocks.size()]);
+        System.out.println(stockArray.length);
+        System.out.println(stocks.size());
+        String[] dates = stocks.stream().map(t -> t.getDate()).toArray(String[]::new);
+        Double[] closing = stocks.stream().map(t -> t.getClose()).toArray(Double[]::new);
+        Double[] money = stocks.stream().map(t -> t.getClose() * numberOfStock).toArray(Double[]::new);
+        System.out.println(dates.length);
+        System.out.println(closing.length);
+        model.addAttribute("stock", stockArray);
+        model.addAttribute("money", money);
+        model.addAttribute("dates", dates);
+        model.addAttribute("closing", closing);
+        model.addAttribute("numberOfStock", numberOfStock);
+
+
+        List<Stock> highestOpen = dao.highestOpen("spy", startDate, endDate);
+        List<Stock> highestVol = dao.highestVol("spy", startDate, endDate);
+        List<Stock> highestClose = dao.highestClose("spy", startDate, endDate);
+        List<Stock> biggestIncrease = dao.biggestIncrease("spy", startDate, endDate);
+        List<Stock> biggestDecrease = dao.biggestDecrease("spy", startDate, endDate);
+        model.addAttribute("stocks", stocks);
+        model.addAttribute("highestOpen", highestOpen);
+        model.addAttribute("highestVol", highestVol);
+        model.addAttribute("highestClose", highestClose);
+        model.addAttribute("biggestIncrease", biggestIncrease);
+        model.addAttribute("biggestDecrease", biggestDecrease);
+
+
+        return "iwm";
+
+    }
+
+    @GetMapping(path="/vti")
+    public String getVolVTI (Model model, @RequestParam("startdate") String startdate, @RequestParam("enddate") String  enddate, @RequestParam("amount") String amount) throws ParseException {
+        double value = Double.parseDouble(amount);
+        List<StockInfo> stockInfo = dao.getStockInfo("vti");
+        model.addAttribute("stockInfo", stockInfo.get(0));
+
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = format.parse(startdate);
+        Date endDate = format.parse(enddate);
+        List<Stock> stocks = dao.list("vti",startDate, endDate);
+
+        Stock firstday = dao.get("vti", startDate);
+        double numberOfStock = value/firstday.getClose();
+
+        Stock[] stockArray = stocks.toArray(new Stock[stocks.size()]);
+        System.out.println(stockArray.length);
+        System.out.println(stocks.size());
+        String[] dates = stocks.stream().map(t -> t.getDate()).toArray(String[]::new);
+        Double[] closing = stocks.stream().map(t -> t.getClose()).toArray(Double[]::new);
+        Double[] money = stocks.stream().map(t -> t.getClose() * numberOfStock).toArray(Double[]::new);
+        System.out.println(dates.length);
+        System.out.println(closing.length);
+        model.addAttribute("stock", stockArray);
+        model.addAttribute("money", money);
+        model.addAttribute("dates", dates);
+        model.addAttribute("closing", closing);
+        model.addAttribute("numberOfStock", numberOfStock);
+
+
+        List<Stock> highestOpen = dao.highestOpen("vti", startDate, endDate);
+        List<Stock> highestVol = dao.highestVol("vti", startDate, endDate);
+        List<Stock> highestClose = dao.highestClose("vti", startDate, endDate);
+        List<Stock> biggestIncrease = dao.biggestIncrease("vti", startDate, endDate);
+        List<Stock> biggestDecrease = dao.biggestDecrease("vti", startDate, endDate);
+        model.addAttribute("stocks", stocks);
+        model.addAttribute("highestOpen", highestOpen);
+        model.addAttribute("highestVol", highestVol);
+        model.addAttribute("highestClose", highestClose);
+        model.addAttribute("biggestIncrease", biggestIncrease);
+        model.addAttribute("biggestDecrease", biggestDecrease);
+
+
+        return "iwm";
+
+    }
+
+
 
 
 }
